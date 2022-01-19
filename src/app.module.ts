c46f9daa749a192bafab1f08ec529afd282bb8e5
Felipe Patrício <86684357+felipepatricioo@users.noreply.github.com>
2022-01-19 20:58:17 -0300

    creating the resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
