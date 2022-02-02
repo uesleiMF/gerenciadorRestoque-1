5821fbca4f21fff1c05c8a9ea55138dab3b817df
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-02-02 20:32:43 -0300

    sim

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma.service';
-
+//teste
 @Module({
   providers: [UserService, PrismaService],
   controllers: [UserController],
